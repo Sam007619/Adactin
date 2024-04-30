5f4e3b74a96c5736d563ba700dae3aef397bcf2f
Sam007619 <sujithsam098@gmail.com>
2024-04-30 20:58:44 +0530

    Adactin

---
+++ Reports/Html_Report/report.js
@@ -0,0 +1,326 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Adactin.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Hotel Booking in Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "User Login in the Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.;user-login-in-the-adactin-application.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "User launch the Adactin web application.",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "User enter the user name in the username field.",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "User enter the password in the password field.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User after given the login details click the login button to navigate to search hotel page.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_launch_the_Adactin_web_application()"
+});
+formatter.result({
+  "duration": 9899493800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_user_name_in_the_username_field()"
+});
+formatter.result({
+  "duration": 2246557900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_password_in_the_password_field()"
+});
+formatter.result({
+  "duration": 113322700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_after_given_the_login_details_click_the_login_button_to_navigate_to_search_hotel_page()"
+});
+formatter.result({
+  "duration": 985081400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "User search hotel page in Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.;user-search-hotel-page-in-adactin-application.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 17,
+  "name": "User have to select mandatory for preffered location in search hotel page.",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "User have to select preffered hotels in search hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "User have to select preffered Room Type in search hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "User have to select mandatory for the number of rooms in search hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "User have to select mandatory for adults per room in search hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "User have to select children per room in search hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "After selcted all the fields and click search button to navigate select hotel page.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_mandatory_for_preffered_location_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 257263100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_preffered_hotels_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 150720300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_preffered_Room_Type_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 144629600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_mandatory_for_the_number_of_rooms_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 327098800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_mandatory_for_adults_per_room_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 209048600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_children_per_room_in_search_hotel_page()"
+});
+formatter.result({
+  "duration": 192053100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.after_selcted_all_the_fields_and_click_search_button_to_navigate_select_hotel_page()"
+});
+formatter.result({
+  "duration": 823022000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "User Select hotel page in Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.;user-select-hotel-page-in-adactin-application.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 35,
+  "name": "User have to select mandatory the hotel from the select hotel page.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 37,
+  "name": "User have to click continue button to navigate Book a hotel page.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_select_mandatory_the_hotel_from_the_select_hotel_page()"
+});
+formatter.result({
+  "duration": 115804300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_click_continue_button_to_navigate_Book_a_hotel_page()"
+});
+formatter.result({
+  "duration": 541842600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 41,
+  "name": "User book a hotel in Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.;user-book-a-hotel-in-adactin-application.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 43,
+  "name": "User mandatory to enter the First name in First name field.",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 45,
+  "name": "User mandatory to enter the Last name in Last name field.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 47,
+  "name": "User mandatory to enter the Billing Address in Billing Address field.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 49,
+  "name": "User mandatory to enter the Credit Card No in Credit Card No field.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 51,
+  "name": "User mandatory to select the preffered credit card type to select in credit card type.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 53,
+  "name": "User mandatory to select the Expiry month to select in credit card type.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 55,
+  "name": "User mandatory to select the Expiry year to select in credit card type.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 57,
+  "name": "User mandatory to enter the CVV number to enter in the CVV number.",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 59,
+  "name": "User have to click Search button to navigate Booking Confirmation page.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_enter_the_First_name_in_First_name_field()"
+});
+formatter.result({
+  "duration": 120695400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_enter_the_Last_name_in_Last_name_field()"
+});
+formatter.result({
+  "duration": 120717000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_enter_the_Billing_Address_in_Billing_Address_field()"
+});
+formatter.result({
+  "duration": 167508600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_enter_the_Credit_Card_No_in_Credit_Card_No_field()"
+});
+formatter.result({
+  "duration": 203755500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_select_the_preffered_credit_card_type_to_select_in_credit_card_type()"
+});
+formatter.result({
+  "duration": 264378600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_select_the_Expiry_month_to_select_in_credit_card_type()"
+});
+formatter.result({
+  "duration": 361668000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_select_the_Expiry_year_to_select_in_credit_card_type()"
+});
+formatter.result({
+  "duration": 412043800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_mandatory_to_enter_the_CVV_number_to_enter_in_the_CVV_number()"
+});
+formatter.result({
+  "duration": 165897100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_have_to_click_Search_button_to_navigate_Booking_Confirmation_page()"
+});
+formatter.result({
+  "duration": 106705800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 63,
+  "name": "User booking confirmation page in Adactin Application.",
+  "description": "",
+  "id": "hotel-booking-in-adactin-application.;user-booking-confirmation-page-in-adactin-application.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 65,
+  "name": ": User to click my logout button to navigate Booked logout page.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_to_click_my_logout_button_to_navigate_Booked_logout_page()"
+});
+formatter.result({
+  "duration": 5067925600,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//input[@name\u003d\u0027logout\u0027]\"}\n  (Session info: chrome\u003d124.0.6367.91)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027SAM\u0027, ip: \u0027192.168.1.12\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002711.0.0.1\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 124.0.6367.91, chrome: {chromedriverVersion: 124.0.6367.91 (51df0e5e17a8..., userDataDir: C:\\Users\\sujit\\AppData\\Loca...}, fedcm:accounts: true, goog:chromeOptions: {debuggerAddress: localhost:51084}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: a49a1caf1dea9ee821c5b89178afee53\n*** Element info: {Using\u003dxpath, value\u003d//input[@name\u003d\u0027logout\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.adactin.stepdefinition.StepDefinition.user_to_click_my_logout_button_to_navigate_Booked_logout_page(StepDefinition.java:209)\r\n\tat ✽.Then : User to click my logout button to navigate Booked logout page.(Adactin.feature:65)\r\n",
+  "status": "failed"
+});
+});
